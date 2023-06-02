737eaa8103c327d008a6c7b577213b3b5beee28f
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2023-06-02 13:09:42 +0900

    added LaunchBrowser code changes

--- src/main/java/seleniumsessions/LaunchBrowser.java
+++ src/main/java/seleniumsessions/LaunchBrowser.java
@@ -7,30 +7,30 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class LaunchBrowser {
 
 	public static void main(String[] args) {
-		
-		//open the browser - chrome
-		ChromeDriver driver=new ChromeDriver();
-		
-		//FirefoxDriver driver=new FirefoxDriver();
-		
-		//EdgeDriver driver=new EdgeDriver();
-		
-		//enter the url:
+
+		// open the browser - chrome
+		ChromeDriver driver = new ChromeDriver();
+
+		// FirefoxDriver driver=new FirefoxDriver();
+
+		// EdgeDriver driver=new EdgeDriver();
+
+		// enter the url:
 		driver.get("https://www.google.com/");
-		
-		//get the Title:
-		String title=driver.getTitle();
-		System.out.println("Actual title: " +title);
-		
-		//verify the title: act vs exp result: checkpoint/verification point
-		if(title.equalsIgnoreCase("Google")) {
+
+		// get the Title:
+		String title = driver.getTitle();
+		System.out.println("Actual title: " + title);
+
+		// verify the title: act vs exp result: checkpoint/verification point
+		if (title.equalsIgnoreCase("Google")) {
 			System.out.println("Correct title");
-		}else {
+		} else {
 			System.out.println("In-correct title");
 		}
-		
+
 		driver.quit();
-		
+
 	}
 
 }
