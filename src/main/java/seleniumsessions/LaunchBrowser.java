1e58ebb5fc431aa32c54e4dd0d57607d75681fe1
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2023-06-03 07:40:04 +0900

    added TopCastingOptions changes

--- src/main/java/seleniumsessions/LaunchBrowser.java
+++ src/main/java/seleniumsessions/LaunchBrowser.java
@@ -1,5 +1,6 @@
 package seleniumsessions;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -9,11 +10,13 @@ public class LaunchBrowser {
 	public static void main(String[] args) {
 
 		// open the browser - chrome
-		ChromeDriver driver = new ChromeDriver();
+		//ChromeDriver driver = new ChromeDriver();
 
 		// FirefoxDriver driver=new FirefoxDriver();
 
 		// EdgeDriver driver=new EdgeDriver();
+		
+		WebDriver driver = new ChromeDriver();
 
 		// enter the url:
 		driver.get("https://www.google.com/");
--- src/main/java/seleniumsessions/TopCastingOptions.java
+++ src/main/java/seleniumsessions/TopCastingOptions.java
@@ -1,15 +1,38 @@
 package seleniumsessions;
 
 import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TopCastingOptions {
 
 	public static void main(String[] args) {
 		
+		//0.valid but recommended only for the specific browser
+		ChromeDriver driver=new ChromeDriver();
+		
 		//1.valid but not recommended 
-		SearchContext sc=new ChromeDriver();
-
+		//SearchContext sc=new ChromeDriver();
+		
+		//2. valid and recommended  ---local execution
+		//WebDriver driver =new ChromeDriver();
+		
+		//3. valid and recommended ---local execution
+		//RemoteWebDriver driver=new ChromeDriver();
+		
+		//4. Valid only for chrome/edge 
+		//ChromiumDriver driver=new ChromeDriver();
+		//driver=new EdgeDriver();
+		//driver=new FirefoxDriver();
+		
+		//5. valid and recommended -- used for remote execution/selenium grid/cloud/docker -aws/browser stack/LambdaTest
+		//WebDriver driver=new RemoteWebDriver(remoteAddress, capabilities);
+		
+		//6.Valid but not recommended 
+		//SearchContext sc=new RemoteWebDriver(remoteAddress, capabilities);
+		
+		
 	}
 
 }
