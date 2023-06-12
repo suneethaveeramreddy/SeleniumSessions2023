f09b6f1c31a660d45ce60fa08c27981d8481cecd
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2023-06-12 10:36:27 +0900

    added CustomCssSelector Changes

---
+++ src/main/java/seleniumsessions/CustomCssSelectorConcept.java
@@ -0,0 +1,14 @@
+package seleniumsessions;
+
+public class CustomCssSelectorConcept {
+
+	public static void main(String[] args) {
+		
+		//id --> #id 
+		//tag#id
+		
+		//class --> .class
+		
+	}
+
+}
