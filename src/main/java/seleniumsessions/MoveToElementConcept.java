89c0c5e9d17289cd73b19f81794ac580efc654c0
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2023-06-16 13:29:34 +0900

    added DragAndDropConcept changes

--- src/main/java/seleniumsessions/MoveToElementConcept.java
+++ src/main/java/seleniumsessions/MoveToElementConcept.java
@@ -1,6 +1,7 @@
 package seleniumsessions;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -8,7 +9,7 @@ import org.openqa.selenium.interactions.Actions;
 public class MoveToElementConcept {
 
 	public static void main(String[] args) throws InterruptedException {
-		ChromeDriver driver=new ChromeDriver();
+		WebDriver driver=new ChromeDriver();
 		driver.get("http://mrbool.com/search/?txtsearch=how-to-create-menu-with-submenu-using-css-html");
 		driver.manage().window().maximize();
 		Thread.sleep(3000);
