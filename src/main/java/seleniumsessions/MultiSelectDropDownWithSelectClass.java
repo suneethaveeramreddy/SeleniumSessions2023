3c111e540c2db24abe5dbe462e51489a4d320fb6
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2023-06-21 11:02:58 +0900

    updated MultiSelectDropDownWithSelectClass changes

--- src/main/java/seleniumsessions/MultiSelectDropDownWithSelectClass.java
+++ src/main/java/seleniumsessions/MultiSelectDropDownWithSelectClass.java
@@ -8,15 +8,22 @@ import org.openqa.selenium.support.ui.Select;
 
 public class MultiSelectDropDownWithSelectClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://html.com/attributes/select-multiple/");
 		WebElement dropdownEle=driver.findElement(By.xpath("//select"));
 		Select select=new Select(dropdownEle);
 		if(select.isMultiple()) {
 			System.out.println("multi select is possible");
+			select.selectByVisibleText("American flamingo");
+			select.selectByVisibleText("Chilean flamingo");
+			select.selectByVisibleText("James's flamingo");
+			select.selectByVisibleText("Lesser flamingo");
 		}
-		
+		Thread.sleep(3000);
+		select.deselectByVisibleText("American flamingo");
+		Thread.sleep(3000);
+		select.deselectAll();
 
 	}
 
