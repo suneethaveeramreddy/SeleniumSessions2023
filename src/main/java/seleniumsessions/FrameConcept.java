7db06d14cd584cba89689e369440e705e2469cb7
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2023-06-22 11:42:44 +0900

    added FrameHandle changes

---
+++ src/main/java/seleniumsessions/FrameConcept.java
@@ -0,0 +1,19 @@
+package seleniumsessions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FrameConcept {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://classic.crmpro.com/");
+		Thread.sleep(3000);
+		
+		driver.findElement(By.name("username")).sendKeys("suneeethav");
+		driver.findElement(By.name("password")).sendKeys("Secure#12");
+		driver.findElement(By.xpath("//input[@type='submit']")).click();
+	}
+
+}
