96319f3036c469ae3569d2a27a1912b54259f726
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2023-06-26 05:42:08 +0900

    updated YatraFramePopUp changes

--- src/main/java/seleniumsessions/YatraFramePopUp.java
+++ src/main/java/seleniumsessions/YatraFramePopUp.java
@@ -1,5 +1,6 @@
 package seleniumsessions;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -11,8 +12,13 @@ public class YatraFramePopUp {
 		driver.get("https://www.yatra.com/");//page
 		Thread.sleep(3000);
 		
-		//driver.switchTo().frame("")
+		driver.switchTo().frame("webklipper-publisher-widget-container-notification-frame");
+		driver.findElement(By.xpath("//i[@class='wewidgeticon we_close']")).click();
 		
+		driver.switchTo().defaultContent();
+		Thread.sleep(4000);
+		
+		driver.findElement(By.xpath("//button[text()='Ok,I Agree']")).click();
 	
 
 	}
