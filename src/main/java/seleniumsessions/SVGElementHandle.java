b484d6edc9642e5a2a7ba5e685ddc46e14a69d42
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2023-06-28 19:18:45 +0900

    updated SVGElementHandle changes

--- src/main/java/seleniumsessions/SVGElementHandle.java
+++ src/main/java/seleniumsessions/SVGElementHandle.java
@@ -9,21 +9,34 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class SVGElementHandle {
-		static WebDriver driver;
+
+	// SVG - normal xpath wont work
+	// parent svg: local-name()
+	// child svg: name()
+	// css selector : no support 
+	// can we use other attributes: yes we can use it with "AND" and "OR" operator
+
+	static WebDriver driver;
+
 	public static void main(String[] args) throws InterruptedException {
-		driver= new ChromeDriver();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.get("https://petdiseasealerts.org/forecast-map/");
 		Thread.sleep(3000);
 		driver.switchTo().frame(driver.findElement(By.xpath("//iframe[contains(@id,'map-instance')]")));
-		List<WebElement> statesList = driver
-				.findElements(By.xpath("//*[local-name()='svg' and @id='map-svg']//*[name()='g' and @id='regions']//*[name()='path']")); 
+		List<WebElement> statesList = driver.findElements(By
+				.xpath("//*[local-name()='svg' and @id='map-svg']//*[name()='g' and @id='regions']//*[name()='path']"));
 		System.out.println(statesList.size());
-		Actions act=new Actions(driver);
-		for (WebElement e:statesList) {
-			act.moveToElement(e).perform();
-			Thread.sleep(500);
-			String statenames = e.getAttribute("name");
-			System.out.println(statenames);
+		// Actions act=new Actions(driver);
+		for (WebElement e : statesList) {
+			// act.moveToElement(e).build().perform();
+			// Thread.sleep(5000);
+			String statename = e.getAttribute("name");
+			System.out.println(statename);
+			if (statename.contains("Maryland")) {
+				e.click();
+				break;
+			}
 		}
 	}
 }
