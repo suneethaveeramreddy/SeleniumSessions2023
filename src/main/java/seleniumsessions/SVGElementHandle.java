4f7f4a87e2f9b68b5c0d05fcc2f91c3085671646
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2023-06-28 11:38:48 +0900

    updated SVGElementHandle changes

--- src/main/java/seleniumsessions/SVGElementHandle.java
+++ src/main/java/seleniumsessions/SVGElementHandle.java
@@ -1,14 +1,29 @@
 package seleniumsessions;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class SVGElementHandle {
 		static WebDriver driver;
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		driver= new ChromeDriver();
 		driver.get("https://petdiseasealerts.org/forecast-map/");
-
+		Thread.sleep(3000);
+		driver.switchTo().frame(driver.findElement(By.xpath("//iframe[contains(@id,'map-instance')]")));
+		List<WebElement> statesList = driver
+				.findElements(By.xpath("//*[local-name()='svg' and @id='map-svg']//*[name()='g' and @id='regions']//*[name()='path']")); 
+		System.out.println(statesList.size());
+		Actions act=new Actions(driver);
+		for (WebElement e:statesList) {
+			act.moveToElement(e).perform();
+			Thread.sleep(500);
+			String statenames = e.getAttribute("name");
+			System.out.println(statenames);
+		}
 	}
-
 }
