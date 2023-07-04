fa528733ee19796b44f21381be3d85e4cba4a178
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2023-07-05 06:48:44 +0900

    Updated CheckPresenceOfElementConcept changes

--- src/main/java/seleniumsessions/CheckPresenceOfElementConcept.java
+++ src/main/java/seleniumsessions/CheckPresenceOfElementConcept.java
@@ -1,9 +1,22 @@
 package seleniumsessions;
 
-public class CheckPresenceOfElementConcept {
+import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class CheckPresenceOfElementConcept {
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		By username=By.name("username");
+		By password=By.name("password");
+		By loginBtn=By.xpath("//button[@type='submit']");
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10);
 
 	}
 
