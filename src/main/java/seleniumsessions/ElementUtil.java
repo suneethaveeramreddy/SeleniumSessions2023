da46eda3e803611753d775896dc981854f434733
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2023-07-06 11:35:45 +0900

    Updated CheckPresenceOfElementConcept changes

--- src/main/java/seleniumsessions/ElementUtil.java
+++ src/main/java/seleniumsessions/ElementUtil.java
@@ -1,5 +1,6 @@
 package seleniumsessions;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,7 +8,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ElementUtil {
 
@@ -129,6 +132,31 @@ public class ElementUtil {
 		act.moveToElement(getElement(level1MenuLocator)).perform();
 		Thread.sleep(3000);
 		getElement(level2MenuLocator).click(); // need to replace with doClick method
+		
+	}
+	//*****************************ElementUtil********************************************//
+	/**
+	 * An expectation for checking that an element is present on the DOM of a page. 
+	 * This does not necessarily mean that the element is visible.
+	 * @param locator
+	 * @param timeOut
+	 * @return
+	 */
+	public WebElement waitForElementPresence(By locator, int timeOut) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeOut));
+		return wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+	}
+	/**
+	 * An expectation for checking that an element is present on the DOM of a page and visible.
+	 * Visibility means that the element is not only displayed
+	 * but also has a height and width that is greater than 0.
+	 * @param locator
+	 * @param timeOut
+	 * @return
+	 */
+	public WebElement waitForElementVisible(By locator, int timeOut) {
+		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(timeOut));
+		return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
 		
 	}
 
